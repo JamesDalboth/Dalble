c889a4dc70b28c5d222a1b2540ea7bcc591bd4c6
JamesDalboth <james.dalboth@gmail.com>
2022-02-06 11:54:34 +0000

    Percentage correct

--- src/game/Stats.js
+++ src/game/Stats.js
@@ -5,9 +5,20 @@ import { GameContext } from './Game';
 function Stats() {
   const { state, setState } = useContext(GameContext);
 
+  function percentageCorrect() {
+    var totalCorrect = 0;
+
+    for (var i = 0; i < state.stats.success.length; i++) {
+      totalCorrect += state.stats.success[i];
+    }
+
+    const percCorrect = totalCorrect * 100 / (totalCorrect + state.stats.fail);
+    return percCorrect;
+  }
+
   return (
     <div className="Stats">
-      {}
+      {percentageCorrect() + "% Correct"}
     </div>
   );
 }
