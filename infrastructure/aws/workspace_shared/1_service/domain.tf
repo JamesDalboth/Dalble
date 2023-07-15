bdabd5100eb543f14ee56c26880980052dfeaed6
JamesDalboth <james.dalboth@gmail.com>
2023-07-15 20:31:29 +0100

    fix: acm validation
    [s3/cloudfront]

--- infrastructure/aws/workspace_shared/1_service/domain.tf
+++ infrastructure/aws/workspace_shared/1_service/domain.tf
@@ -20,22 +20,45 @@ module "zones" {
   }
 }
 
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "${var.product}.dalboth.com"
+  validation_method = "DNS"
+
+  tags = {
+    env     = var.env
+    region  = var.region
+    product = var.product
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
   version = "~> 2.0"
 
   zone_name = module.zones.route53_zone_name["dalboth.com"]
 
-  records = [
+  records = concat([
     {
-      name = "dalble"
+      name = var.product
       type = "A"
       alias = {
         name    = module.website.cloudfront_distribution_domain_name
         zone_id = module.website.cloudfront_distribution_hosted_zone_id
       }
     }
-  ]
+    ], [for dvo in aws_acm_certificate.cert.domain_validation_options :
+    {
+      name               = dvo.resource_record_name
+      type               = dvo.resource_record_type
+      ttl                = 60
+      full_name_override = true
+      records            = [dvo.resource_record_value]
+    }
+  ])
 
   depends_on = [module.zones]
 }
